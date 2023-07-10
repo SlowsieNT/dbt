5e2ca78799a2ed38087f5e2b17235898efd3c913
Andrew <86121252+SlowsieNT@users.noreply.github.com>
2023-07-10 19:29:17 +0000

    Update

---
+++ src/inc/lib/ReqHelpers-codegen.js
@@ -0,0 +1,43 @@
+function GenerateFunc(aType, aFName, aArgc, aFuncArgs, aFuncBody){
+	var fout = "function " + aFName;
+	if (!aArgc || aArgc < 1)
+		aArgc = 101;
+	for (var i = 1, r1 = [], r2 = []; i < aArgc; i++) {
+		r1.push("$aKey"+i+" = \"\", &$aOutVar"+i+" = null"),
+		r2.push("$aKey"+i+", $aOutVar"+i);
+	}
+	fout += "(" + aFuncArgs(r1.join(", "))+") {\r\n";
+	fout += aFuncBody(r2.join(", "));
+	fout += "\r\n}";
+	return {
+		a: fout,
+		b: r1,
+		c: r2
+	};
+}
+
+// main function
+copy(GenerateFunc(0, "Request_GPFSC_Get33", 0, function args(aArgsStr) {
+	return "$aType = 0, " + aArgsStr + "";
+}, function body(aArgsStr2) {
+	return "";
+}));
+
+// Create all getters
+var output = [];
+var types = "Get,Post,File,Session,Cookie".split(",");
+for (var i in types) {
+	var fn = "G" + types[i] + "33";
+	output.push(
+		"// Getter for $_" + types[i].toUpperCase() + "\r\n"+
+		GenerateFunc(i, fn, 0, function args(aArgsStr) {
+			return "" + aArgsStr + "";
+		}, function body(aArgsStr2) {
+			return (
+				//"\t\r\n"+
+				"\treturn Request_GPFSC_Get33("+i+", "+aArgsStr2+");"
+			);
+		}).a
+	);
+}
+copy(output.join("\r\n"));
